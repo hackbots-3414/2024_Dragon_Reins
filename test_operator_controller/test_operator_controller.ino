30bf4c9ecee401f32a7b57b639b3e3ac7d748be5
Nolan Barker <107814443+TheRekrab@users.noreply.github.com>
2025-01-25 14:57:27 -0500

    Working on matrix

--- test_operator_controller/test_operator_controller.ino
+++ test_operator_controller/test_operator_controller.ino
@@ -1,14 +1,39 @@
 // #include <Gamepad.h>
 #include <Joystick.h>
 
+
+
+int row_0 = 0;
+int row_1 = 1;
+int row_2 = 2;
+
+int col_0 = 3;
+int col_1 = 4;
+int col_2 = 5;
+
+int pins[6] = {row_0, row_1, row_2, col_0, col_1, col_2};
+int rows[3] = {row_0, row_1, row_2};
+int cols[3] = {col_0, col_1, col_2};
+
+void init_pins(){
+  for (int i = 0; i < 6; i++) {
+    pinMode(pins[i, INPUT_PULLUP]);
+  }
+}
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode(0, INPUT_PULLUP);
-  pinMode(1, INPUT_PULLUP);
-  pinMode(2, INPUT_PULLUP);
-  pinMode(3, INPUT_PULLUP);
+  init_pins();
   Joystick.begin();
   // Serial.begin(9600);
+}
+
+void read_pins() {
+  for (int i = 0; i < 4; i++){
+    for (int j = 0; j < 4; j++){
+      
+    }
+  }
 }
 
 void loop() {
