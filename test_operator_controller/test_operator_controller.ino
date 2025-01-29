63a165e20d61b7b2dcdb7ddb059b95954a59cb95
Nolan Barker <107814443+TheRekrab@users.noreply.github.com>
2025-01-29 17:53:45 -0500

    Working on new matrix style

--- test_operator_controller/test_operator_controller.ino
+++ test_operator_controller/test_operator_controller.ino
@@ -7,9 +7,9 @@ int row_0 = 0;
 int row_1 = 1;
 int row_2 = 2;
 
-int col_0 = 20;
+int col_0 = 18;
 int col_1 = 19;
-int col_2 = 18;
+int col_2 = 20;
 
 int pins[6] = {row_0, row_1, row_2, col_0, col_1, col_2};
 int rows[3] = {row_0, row_1, row_2};
@@ -21,9 +21,13 @@ int ids[3][3] = {
   {7,8,9}
 };
 
+
 void init_pins(){
-  for (int i = 0; i < 6; i++) {
-    pinMode(pins[i], INPUT_PULLUP);
+  for (int i = 0; i < 3; i++) {
+    pinMode(cols[i], INPUT_PULLUP);
+  }
+  for (int i = 0; i < 3; i++) {
+    pinMode(rows[i], INPUT);
   }
 }
 
@@ -35,40 +39,29 @@ void setup() {
 }
 
 void read_pins() {
-  // for (int i = 0; i < 3; i++){
-  //   // Serial.print("i ");
-  //   // Serial.println(!digitalRead(rows[i]));
-  //   // if (!digitalRead(cols[i])) {
-  //   char temp[3];
-  //   int index = 0;
-  //   for (int j = 0; j < 3; j++){
-
-  //     // if (!digitalRead(rows[j])){
-  //     //   // Serial.print(i);
-  //     //   Serial.println(j);
-  //     //   Serial.println(ids[i][j]);
-  //     // }
-  //       // Serial.println(digitalRead(rows[j]));
-  //     // temp[i][j] = (!digitalRead(cols[i]) && !digitalRead(rows[j]));
-  //     temp[index] = '0' + (!digitalRead(rows[i]) && !digitalRead(cols[j]));
-  //     index ++;
-  //     // if (!digitalRead(cols[i]) && !digitalRead(rows[j])){
-  //     //   // Serial.println("REAKKKK");
-  //     //   Serial.println(ids[i][j]);
-  //     // }
-  //   }
-  //   Serial.println(String(temp));
-  //   // }
-  // }
+  for (int rowid = 0; rowid < 3; rowid++){
+    // char temp[3];
+    pinMode(rows[rowid], OUTPUT);
+    digitalWrite(rows[rowid], 0);
+    // delay(10);
+    // Serial.println("Running");
+    for (int colid = 0; colid < 3; colid++){
+      
+      // if (!digitalRead(cols[colid])){
+      //   // Serial.println("REAKKKK");
+      //   // Serial.println(ids[colid][rowid]);
+      // }
+      Joystick.button(ids[colid][rowid], !digitalRead(cols[colid]));
+      // while(!digitalRead(cols[colid]));
+    }
+    digitalWrite(rows[rowid], 1);
+    pinMode(rows[rowid], INPUT);
+    delay(10);
+    // Serial.println(String(temp));
+    // }
+  }
   // Serial.println("\n\n\n\n\n\n\n\n\n\n\n");
-
-  Serial.print(!digitalRead(cols[0]));
-  Serial.print(!digitalRead(cols[1]));
-  Serial.println(!digitalRead(cols[2]));
-  Serial.println(!digitalRead(rows[0]));
-  Serial.println(!digitalRead(rows[1]));
-  Serial.println(!digitalRead(rows[2]));
-  delay(1000);
+  // delay(1000);
 }
 
 void loop() {
